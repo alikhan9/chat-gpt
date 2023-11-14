cd1c73cbf4272a328daee910aa4cebc3426a0cd4
alikhan9 <alikhan.zaipoulaiev@outlook.fr>
2023-11-14 19:39:43 +0100

    "responsive"

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,6 +4,7 @@ import { ref, onMounted, watch } from 'vue'
 import OpenAI from "openai";
 import SvgIcon from '@jamescoyle/vue-icon'
 import { mdiRobot, mdiAlphaABox, mdiSend, mdiStop } from '@mdi/js'
+import { useWindowSize } from '@vueuse/core'
 
 const props = defineProps({
     chatSettings: Object
@@ -35,6 +36,7 @@ const chatRoom = ref(null);
 const enableInput = ref(false);
 const textAreaReff = ref()
 const currentStream = ref(null);
+const { height } = useWindowSize()
 
 
 const saveInDatabase = (role, content) => {
@@ -80,10 +82,18 @@ const stopStream = () => {
 
 const autoResize = () => {
     textAreaReff.value.style.height = 'auto';
-    if (textAreaReff.value.scrollHeight <= 200) {
-        textAreaReff.value.style.height = `${textAreaReff.value.scrollHeight}px`;
-    } else {
-        textAreaReff.value.style.height = '200px';
+    if(height.value > 895){
+        if (textAreaReff.value.scrollHeight <= 200) {
+            textAreaReff.value.style.height = textAreaReff.value.scrollHeight - 38 + 'px';
+        } else {
+            textAreaReff.value.style.height = '180px';
+        }
+    }else {
+        if (textAreaReff.value.scrollHeight <= 200) {
+            textAreaReff.value.style.height = textAreaReff.value.scrollHeight - 38 + 'px';
+        } else {
+            textAreaReff.value.style.height = '15vh';
+        }
     }
 }
 
@@ -92,32 +102,30 @@ const autoResize = () => {
 <template>
     <Head title="ChatGpt" />
     <div class="text-white">
-        <div class=" min-h-[98vh] relative">
-            <div class="flex flex-col">
-                <div class="min-h-[76vh]">
-                    <div v-if="fullChat.length > 0"
-                        :class="{ 'pb-36 mt-[40px] sm:mt-0 scrollbar-hide sm:scrollbar-default h-[70vh] sm:h-[76vh] border-b overflow-x-hidden overflow-auto w-screen sm:w-full': true, '': fullChat.length == 0 }">
-                        <div class=" flex flex-col justify-center">
-                            <div class="mb-2" v-for="(item, index) in fullChat" :key="index">
-                                <div
-                                    :class="{ 'bg-[hsl(0,0%,15%)]': !item.role == 'assistant', 'bg-[hsl(0,0%,30%)]': item.role == 'assistant' }">
-                                    <div v-if="item.role == 'assistant'" class="p-2 rounded">
-                                        <div class="flex items-start justify-center gap-4">
-                                            <svg-icon type="mdi" :path="mdiRobot" size="32"
-                                                class="min-w-[10%] text-red-500"></svg-icon>
-                                            <div class="w-full">
-                                                {{ item.content }}
-                                            </div>
+        <div class="relative max-h-screen overflow-hidden">
+            <div class="flex flex-col justify-between h-screen">
+                <div v-if="fullChat.length > 0"
+                    :class="{ ' mt-[40px] sm:mt-0 scrollbar-hide sm:scrollbar-default  max-h-[80vh] border-b overflow-x-hidden overflow-auto w-screen sm:w-full': true, '': fullChat.length == 0 }">
+                    <div class=" flex flex-col justify-center">
+                        <div class="mb-2" v-for="(item, index) in fullChat" :key="index">
+                            <div
+                                :class="{ 'bg-[hsl(0,0%,15%)]': !item.role == 'assistant', 'bg-[hsl(0,0%,30%)]': item.role == 'assistant' }">
+                                <div v-if="item.role == 'assistant'" class="p-2 rounded">
+                                    <div class="flex items-start justify-center gap-4">
+                                        <svg-icon type="mdi" :path="mdiRobot" size="32"
+                                            class="min-w-[10%] text-red-500"></svg-icon>
+                                        <div class="w-full">
+                                            {{ item.content }}
                                         </div>
                                     </div>
-                                    <div v-else class="rounded min-w-full p-2">
-                                        <div class="flex items-start justify-center gap-4">
-                                            <svg-icon type="mdi" size="32" :path="mdiAlphaABox"
-                                                class="min-w-[10%] text-green-500">
-                                            </svg-icon>
-                                            <div class="w-full">
-                                                {{ item.content }}
-                                            </div>
+                                </div>
+                                <div v-else class="rounded min-w-full p-2">
+                                    <div class="flex items-start justify-center gap-4">
+                                        <svg-icon type="mdi" size="32" :path="mdiAlphaABox"
+                                            class="min-w-[10%] text-green-500">
+                                        </svg-icon>
+                                        <div class="w-full">
+                                            {{ item.content }}
                                         </div>
                                     </div>
                                 </div>
@@ -125,10 +133,10 @@ const autoResize = () => {
                         </div>
                     </div>
                 </div>
-                <div class=" px-4 w-full gap-4 flex bottom-0 py-4 bg-[hsl(0,0%,10%)] h-[15vh] sm:h-[23vh]"
+                <div class=" px-4 w-full min-h-[90px] gap-6 mb-2 flex bottom-0 py-4 bg-[hsl(0,0%,10%)] h-full"
                     :aria-disabled="enableInput">
                     <div class="flex gap-4 w-full items-end mx-4 sm:mx-0 ">
-                        <div class="flex rounded border relative  w-full overflow-auto max-h-[150px] sm:max-h-[400px] ">
+                        <div class="flex rounded border relative  w-full overflow-auto ">
                             <textarea :rows="3" ref="textAreaReff" id="message" type="text" @input="autoResize"
                                 @keydown.enter.exact="send" @keydown.enter.shift.exact="text += '\n'" rows="1"
                                 class=" border  h-[50px] w-full border-none  focus:ring-0  bg-transparent "
