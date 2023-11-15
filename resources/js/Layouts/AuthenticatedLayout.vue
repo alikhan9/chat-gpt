72ef600af47be137668b2a2850e996e63b967be2
alikhan9 <alikhan.zaipoulaiev@outlook.fr>
2023-11-15 02:29:59 +0100

    animation on menu

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -85,8 +85,6 @@ const sendUpdateRoomName = () => {
             roomToEdit.value = { name: '', id: '' };
         }
     });
-    if (showMenu.value)
-        toggleMenu();
 }
 
 const editRoom = room => {
@@ -115,98 +113,140 @@ const toggleMenu = () => {
 </script>
 
 <template>
-    <div class=" bg-[hsl(0,0%,10%)] fixed flex inset-0">
-        <div v-if="width > 640 ? false : true"
-            :class="{ 'fixed top-0 p-5 left-0 z-50 justify-between h-[40px] items-center flex w-full bg-[hsl(0,0%,30%)]': true, 'hidden': showMenu }">
-            <svg-icon @click="toggleMenu" type="mdi" class="text-white hover:cursor-pointer" size="32"
-                :path="mdiMenu"></svg-icon>
-            <div class="text-white text-lg">
-                {{ activeChatRoom?.name }}
-            </div>
-            <svg-icon @click="sendCreateRoom" type="mdi" class="text-white hover:cursor-pointer" size="32"
-                :path="mdiPlus"></svg-icon>
-        </div>
-        <div v-if="width > 640 ? true : showMenu"
-            class=" bg-[hsl(0,0%,5%)] shrink-0 h-full w-full text-white text-xs sm:text-base lg:w-[320px] sm:w-[260px] p-2">
-            <div class="flex flex-col h-full lg:p-4 p-4">
-                <div class="flex gap-2">
-                    <PrimaryButton @click="sendCreateRoom"
-                        class="w-full text-start border p-2 flex border-[hsl(0,0%,25%)] rounded gap-2 items-center hover:border-[hsl(0,0%,30%)] hover:scale-[1.01]">
-                        <svg-icon type="mdi" :path="mdiPlus"></svg-icon>
-                        <div>Nouveau Chat</div>
-                    </PrimaryButton>
-                    <PrimaryButton class="w-full border-[hsl(0,0%,20%)] hover:border-white sm:hidden" @click="toggleMenu">
-                        Fermer</PrimaryButton>
+    <Transition>
+        <div class=" bg-[hsl(0,0%,10%)] fixed flex inset-0">
+            <div v-if="width > 640 ? false : true"
+                :class="{ 'fixed top-0 p-5 left-0 z-50 justify-between h-[40px] items-center flex w-full bg-[hsl(0,0%,30%)]': true, 'hidden': showMenu }">
+                <svg-icon @click="toggleMenu" type="mdi" class="text-white hover:cursor-pointer" size="32"
+                    :path="mdiMenu"></svg-icon>
+                <div class="text-white text-lg">
+                    {{ activeChatRoom?.name }}
                 </div>
-                <div class="flex-col flex-1 overflow-auto scrollbar-hide md:scrollbar-default">
-                    <div v-for="( room, index ) in  chatRooms " :key="index">
-                        <div
-                            :class="{ 'flex items-center p-2 rounded mt-2': true, 'bg-[hsl(0,0%,30%)]': room.id === activeChatRoom?.id }">
-                            <Link href="/" :class="{ 'hidden': room.id == activeChatRoom.id }" :data="{ chatRoom: room.id }"
-                                class="flex gap-4 items-center  min-w-[80%]">
-                            <svg-icon type="mdi" size="20" :path="mdiMessageOutline"></svg-icon>
-                            <div :class="{ 'min-w-[80%]': true, 'hidden': editRoomName && room.id == activeChatRoom.id }">{{
-                                room.name }}</div>
-                            <input ref="nameLink" @keydown.enter="sendUpdateRoomName" v-model="roomToEdit.name"
-                                :class="{ 'min-w-[80%] text-xs sm:text-base p-0 m-0 border-none border-b bg-transparent text-white focus:ring-0 focus:outline-none': true, 'hidden': !editRoomName || room.id !== activeChatRoom.id }" />
-                            </Link>
-                            <div
-                                :class="{ 'hidden': room.id !== activeChatRoom.id, 'flex gap-4 items-center  min-w-[80%]': true }">
-                                <svg-icon type="mdi" size="20" :path="mdiMessageOutline"></svg-icon>
+                <svg-icon @click="sendCreateRoom" type="mdi" class="text-white hover:cursor-pointer" size="32"
+                    :path="mdiPlus"></svg-icon>
+            </div>
+            <Transition name="slide">
+                <div v-if="width > 640 ? true : showMenu"
+                    class=" bg-[hsl(0,0%,5%)] h-full w-full text-white text-xs sm:text-base lg:w-[320px] sm:w-[260px] p-2">
+                    <div class="flex flex-col h-full lg:p-4 p-4">
+                        <div class="flex gap-2">
+                            <PrimaryButton @click="sendCreateRoom"
+                                class="w-full text-start border p-2 flex border-[hsl(0,0%,25%)] rounded gap-2 items-center hover:border-[hsl(0,0%,30%)] hover:scale-[1.01]">
+                                <svg-icon type="mdi" :path="mdiPlus"></svg-icon>
+                                <div>Nouveau Chat</div>
+                            </PrimaryButton>
+                            <PrimaryButton class="w-full border-[hsl(0,0%,20%)] hover:border-white sm:hidden"
+                                @click="toggleMenu">
+                                Fermer</PrimaryButton>
+                        </div>
+                        <div class="flex-col flex-1 overflow-auto scrollbar-hide md:scrollbar-default">
+                            <div v-for="( room, index ) in  chatRooms " :key="index">
                                 <div
-                                    :class="{ 'min-w-[80%] truncate': true, 'hidden': editRoomName && room.id == activeChatRoom.id }">
-                                    {{ room.name }}
-                                </div>
-                                <input ref="nameDiv" @keydown.enter="sendUpdateRoomName"
-                                    :class="{ 'min-w-[80%]  text-xs sm:text-base  p-0 m-0 border-none border-b bg-transparent text-white focus:ring-0 focus:outline-none ': true, 'hidden': !editRoomName || room.id !== activeChatRoom.id }"
-                                    v-model="roomToEdit.name" />
-                            </div>
-                            <div v-if="room.id == activeChatRoom.id" class="relative min-w-[80px] h-full">
-                                <div v-if="!editRoomName" class="absolute -top-[11px] flex items-center gap-2">
-                                    <svg-icon @click="() => editRoom(room)" class="hover:cursor-pointer" type="mdi"
-                                        size="20" :path="mdiFileEditOutline"></svg-icon>
-                                    <svg-icon class="hover:cursor-pointer" @click="() => sendDeleteRoom(room.id)" type="mdi"
-                                        size="20" :path="mdiDeleteOutline"></svg-icon>
-                                </div>
-                                <div v-else class="absolute min-w-[80px] -top-[11px] flex items-center gap-2">
-                                    <svg-icon @click="sendUpdateRoomName" class="hover:cursor-pointer" type="mdi" size="20"
-                                        :path="mdiCheckBold"></svg-icon>
-                                    <svg-icon @click="closeEditRoom" class="hover:cursor-pointer" type="mdi" size="20"
-                                        :path="mdiClose"></svg-icon>
+                                    :class="{ 'flex items-center p-2 rounded mt-2': true, 'bg-[hsl(0,0%,30%)]': room.id === activeChatRoom?.id }">
+                                    <Link href="/" :class="{ 'hidden': room.id == activeChatRoom.id }"
+                                        @click="() => showMenu ? toggleMenu() : null" :data="{ chatRoom: room.id }"
+                                        class="flex gap-4 items-center  min-w-[80%]">
+                                    <svg-icon type="mdi" size="20" :path="mdiMessageOutline"></svg-icon>
+                                    <div
+                                        :class="{ 'min-w-[80%]': true, 'hidden': editRoomName && room.id == activeChatRoom.id }">
+                                        {{
+                                            room.name }}</div>
+                                    <input ref="nameLink" @keydown.enter="sendUpdateRoomName" v-model="roomToEdit.name"
+                                        :class="{ 'min-w-[80%] text-xs sm:text-base p-0 m-0 border-none border-b bg-transparent text-white focus:ring-0 focus:outline-none': true, 'hidden': !editRoomName || room.id !== activeChatRoom.id }" />
+                                    </Link>
+                                    <div
+                                        :class="{ 'hidden': room.id !== activeChatRoom.id, 'flex gap-4 items-center  min-w-[80%]': true }">
+                                        <svg-icon type="mdi" size="20" :path="mdiMessageOutline"></svg-icon>
+                                        <div
+                                            :class="{ 'min-w-[80%] truncate': true, 'hidden': editRoomName && room.id == activeChatRoom.id }">
+                                            {{ room.name }}
+                                        </div>
+                                        <input ref="nameDiv" @keydown.enter="sendUpdateRoomName"
+                                            :class="{ 'min-w-[80%]  text-xs sm:text-base  p-0 m-0 border-none border-b bg-transparent text-white focus:ring-0 focus:outline-none ': true, 'hidden': !editRoomName || room.id !== activeChatRoom.id }"
+                                            v-model="roomToEdit.name" />
+                                    </div>
+                                    <div v-if="room.id == activeChatRoom.id" class="relative min-w-[80px] h-full">
+                                        <div v-if="!editRoomName" class="absolute -top-[11px] flex items-center gap-2">
+                                            <svg-icon @click="() => editRoom(room)" class="hover:cursor-pointer" type="mdi"
+                                                size="20" :path="mdiFileEditOutline"></svg-icon>
+                                            <svg-icon class="hover:cursor-pointer" @click="() => sendDeleteRoom(room.id)"
+                                                type="mdi" size="20" :path="mdiDeleteOutline"></svg-icon>
+                                        </div>
+                                        <div v-else class="absolute min-w-[80px] -top-[11px] flex items-center gap-2">
+                                            <svg-icon @click="sendUpdateRoomName" class="hover:cursor-pointer" type="mdi"
+                                                size="20" :path="mdiCheckBold"></svg-icon>
+                                            <svg-icon @click="closeEditRoom" class="hover:cursor-pointer" type="mdi"
+                                                size="20" :path="mdiClose"></svg-icon>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="shrink-0 bg-[hsl(0,0%,5%)]">
-                    <div class="mb-4">
-                        <div class="font-bold text-base sm:text-xl">Model</div>
-                        <select v-on:change="sendUpdateChatSettings"
-                            class="bg-[hsl(0,0%,20%)] text-white border-none text-xs sm:text-base w-full rounded focus:ring-0 focus:outline-none"
-                            v-model="chatSettings.model">
-                            <option class="text-center" v-for=" model  in  models ">
-                                {{ model.name }}
-                            </option>
-                        </select>
-                    </div>
-                    <div class="mb-4">
-                        <div class="font-bold  text-base sm:text-xl">Température</div>
-                        <div class="flex items-center justify-between">
-                            <input v-on:change="sendUpdateChatSettings" v-model="chatSettings.temperature" class="w-[80%]"
-                                type="range" min="0" max="2" step="0.1" />
-                            <div class="text-base sm:text-lg">{{ chatSettings.temperature }}</div>
+                        <div class="shrink-0 bg-[hsl(0,0%,5%)]">
+                            <div class="mb-4">
+                                <div class="font-bold text-base sm:text-xl">Model</div>
+                                <select v-on:change="sendUpdateChatSettings"
+                                    class="bg-[hsl(0,0%,20%)] text-white border-none text-xs sm:text-base w-full rounded focus:ring-0 focus:outline-none"
+                                    v-model="chatSettings.model">
+                                    <option class="text-center" v-for=" model  in  models ">
+                                        {{ model.name }}
+                                    </option>
+                                </select>
+                            </div>
+                            <div class="mb-4">
+                                <div class="font-bold  text-base sm:text-xl">Température</div>
+                                <div class="flex items-center justify-between">
+                                    <input v-on:change="sendUpdateChatSettings" v-model="chatSettings.temperature"
+                                        class="w-[80%]" type="range" min="0" max="2" step="0.1" />
+                                    <div class="text-base sm:text-lg">{{ chatSettings.temperature }}</div>
+                                </div>
+                            </div>
+                            <PrimaryButton href="/logout" method="POST" type="link"
+                                class="w-full text-start border p-2 flex border-[hsl(0,0%,20%)] rounded gap-2 items-center hover:border-[hsl(0,0%,30%)] hover:scale-[1.01]">
+                                <svg-icon type="mdi" :path="mdiLogout"></svg-icon>
+                                <div>Se déconnecter</div>
+                            </PrimaryButton>
                         </div>
                     </div>
-                    <PrimaryButton href="/logout" method="POST" type="link"
-                        class="w-full text-start border p-2 flex border-[hsl(0,0%,20%)] rounded gap-2 items-center hover:border-[hsl(0,0%,30%)] hover:scale-[1.01]">
-                        <svg-icon type="mdi" :path="mdiLogout"></svg-icon>
-                        <div>Se déconnecter</div>
-                    </PrimaryButton>
                 </div>
-            </div>
+            </Transition>
+            <Transition name="fade">
+                <div v-if="width > 640 ? true : !showMenu" :class="{ ' h-full w-full': true }">
+                    <slot />
+                </div>
+            </Transition>
         </div>
-        <div :class="{ 'h-full w-full': true, 'hidden sm:block': showMenu }">
-            <slot />
-        </div>
-    </div>
+    </Transition>
 </template>
+
+<style>
+.slide-enter-active {
+    transition: all 0.3s ease-in-out;
+    transition-delay: 0.3s;
+
+}
+
+.slide-leave-active {
+    transition: all 0.3s ease-in;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+    transform: translateX(-100vw);
+}
+
+.fade-leave-active {
+    transition: all 0.3s ease-in;
+}
+
+
+.fade-enter-active {
+    transition: all 0.3s ease-in-out;
+    transition-delay: 0.3s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    transform: translateX(100vw);
+}
+</style>
